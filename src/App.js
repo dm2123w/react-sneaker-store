3650b39b1a0766b0ad72f48415783bd7536bfaca
dmitriysemenko <dm123semenko@gmail.com>
2021-09-12 00:18:53 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="wrapper">
+            <header>
+                <div className="headerLeft">
+                    <svg />
+                    <div className="headerInfo">
+                        <h3>Sneaker Shop</h3>
+                        <p>Discover the best sneakers</p>
+                    </div>
+                </div>
+                <ul className="headerRight">
+                    <li>
+                        <svg />
+                        <span>$135.00</span>
+                    </li>
+                    <li>
+                        <svg />
+                    </li>
+                </ul>
+            </header>
+            <div className="content">
+                <h1>All Items</h1>
+                ...
+            </div>
+        </div>
+    );
 }
 
 export default App;
