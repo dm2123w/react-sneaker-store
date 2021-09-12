28d716e7f2c7057c0095188cfb4f5d701988522b
dmitriysemenko <dm123semenko@gmail.com>
2021-09-12 22:04:06 +0300

    update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ function App() {
         <div className="wrapper">
             <header>
                 <div className="headerLeft">
-                    <svg />
+                    <img
+                        width={40}
+                        height={40}
+                        src="./icons/logo.png"
+                        alt="logo"
+                    />
                     <div className="headerInfo">
                         <h3>Sneaker Shop</h3>
                         <p>Discover the best sneakers</p>
@@ -11,11 +16,21 @@ function App() {
                 </div>
                 <ul className="headerRight">
                     <li>
-                        <svg />
+                        <img
+                            width={18}
+                            height={18}
+                            src="./icons/cart.svg"
+                            alt="logo"
+                        />
                         <span>$135.00</span>
                     </li>
                     <li>
-                        <svg />
+                        <img
+                            width={18}
+                            height={18}
+                            src="./icons/user.svg"
+                            alt="logo"
+                        />
                     </li>
                 </ul>
             </header>
