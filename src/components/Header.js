b086a62b3796950f2088541d7d51c3c3b416db48
dmitriysemenko <dm123semenko@gmail.com>
2021-09-25 22:24:49 +0300

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,14 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import AppContext from "../context";
 
 function Header(props) {
+    const { cartItems } = useContext(AppContext);
+    const totalPrice = cartItems.reduce(
+        (sum, obj) => Number(obj.price) + Number(sum),
+        0
+    );
+
     return (
         <header className="d-flex justify-between align-center p-40">
             <Link to="/">
@@ -25,7 +33,7 @@ function Header(props) {
                         src="./img/cart.svg"
                         alt="Cart"
                     />
-                    <span>$135.00</span>
+                    <span>${totalPrice}</span>
                 </li>
                 <li className="mr-20 cu-p">
                     <Link to="/favorites" exact>
